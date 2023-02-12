7f0300d6a3c0d77dfce39f5a40c5e8feeee4a379
Memetz45 <slametnurhidayat45@gmail.com>
2023-02-13 00:04:43 +0700

    memasukan gambar dengan link

--- src/reactJs_eduwork/Tugas/component/tugasKomponen.jsx
+++ src/reactJs_eduwork/Tugas/component/tugasKomponen.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
+
 const TugasKomponen = () => {
     return(
         <div class="fungsi">
             <h1>@Slamet Nurhidayat</h1>
-            <img src="/public/favicon.png"/>
+            <img src="https://eduwork.id/assets/favicon.png"/>
         </div>
     )
 }
