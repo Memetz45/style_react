e1065a2bc575f25f7f18afc68672cfa76e5f2746
Memetz45 <slametnurhidayat45@gmail.com>
2023-02-20 10:54:58 +0700

    merapihkan file

---
+++ src/reactJs_eduwork/Tugas/Tugas_Style/Body.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Menu } from "antd";
+import './Body.css';
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { CgProfile } from "react-icons/cg";
+import { IoSchoolSharp } from "react-icons/io5";
+import { GiSkills } from "react-icons/gi";
+import { MdModelTraining } from "react-icons/md";
+import { GrProjects } from "react-icons/gr";
+import Profile from "./Profile";
+import Education from "./Education";
+import Skill from "./Skill"
+import JobTraining from "./JobTraining";
+import Projects from "./Projects";
+import Header from "./Header";
+
+// import { Content } from "antd/es/layout/layout";
+
+
+const Body = () => {
+    const navigate = useNavigate();
+    const style = {
+        backgroundColor: 'aqua',
+        backgroundSize: 'auto'
+    }
+    return (
+        <div>
+            <Header/>
+            <div className="body">
+
+                <Menu style={style}
+                    onClick={({ key }) => {
+                        if (key === "profile") {
+
+                        } else {
+                            navigate(key);
+                        };
+                    }}
+                    items={[
+                        { label: "Profile", key: "/profile", icon: <CgProfile /> },
+                        { label: "Education", key: "/education", icon: <IoSchoolSharp /> },
+                        { label: "Skill", key: "/skill", icon: <GiSkills /> },
+                        { label: "Job Training", key: "/jobTraining", icon: <MdModelTraining /> },
+                        { label: "Projects", key: "/project", icon: <GrProjects /> }
+                    ]}></Menu>
+                <Content />
+            </div>
+        </div>
+    );
+}
+
+function Content() {
+    return (
+        <Routes>
+            <Route path="/profile" element={<Profile />}></Route>
+            <Route path="/education" element={<Education />}></Route>
+            <Route path="/skill" element={<Skill />}></Route>
+            <Route path="/jobTraining" element={<JobTraining />}></Route>
+            <Route path="/project" element={<Projects />}></Route>
+        </Routes>
+    );
+};
+
+export default Body;
--- src/reactJs_eduwork/Tugas/index.jsx
+++ src/reactJs_eduwork/Tugas/index.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Body from "./Tugas_Style/Body";
-import Header from "./Tugas_Style/Header";
 
 class Tugas extends React.Component {
     render(){
         return(
             <div>
-                <Header/>
                 <Body/>
             </div>
         )
